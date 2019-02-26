50a6bd91d94a1bcc0ee521353f91ba5e354415a3
nice79422 <nice79422@naver.com>
2019-02-26 12:26:48 +0900

    NaverLogin

---
+++ API/naverLoginTest/loginTest/src/com/naver/naverlogintutorial/controller/LoginTest.java
@@ -0,0 +1,42 @@
+package com.naver.naverlogintutorial.controller;
+ 
+import java.io.IOException;
+
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.naver.naverlogintutorial.oauth.bo.NaverLoginBO;
+ 
+@Controller
+public class LoginTest {
+ 
+	/* NaverLoginBO */
+	private NaverLoginBO naverLoginBO;
+
+	/* NaverLoginBO */
+	@Autowired
+	private void setNaverLoginBO(NaverLoginBO naverLoginBO){
+		this.naverLoginBO = naverLoginBO;
+	}
+	
+    @RequestMapping("/login")
+    public ModelAndView login(HttpSession session) {
+        /* 네아로 인증 URL을 생성하기 위하여 getAuthorizationUrl을 호출 */
+        String naverAuthUrl = naverLoginBO.getAuthorizationUrl(session);
+        
+        /* 생성한 인증 URL을 View로 전달 */
+        return new ModelAndView("login", "url", naverAuthUrl);
+    }
+    
+	@RequestMapping("/callback")
+	public ModelAndView callback(@RequestParam String code, @RequestParam String state, HttpSession session) throws IOException {
+		/* 네아로 인증이 성공적으로 완료되면 code 파라미터가 전달되며 이를 통해 access token을 발급 */
+		OAuth2AccessToken oauthToken = naverLoginBO.getAccessToken(session, code, state);
+    	return new ModelAndView("callback");
+	}
+}
